e012f6f0714541779d3e6a18313ebde04672b6c4
Daniel <danivictor45i@gmail.com>
2022-07-01 19:20:43 -0300

    faq home filled

--- src/data/faq.js
+++ src/data/faq.js
@@ -1,23 +1,23 @@
 const faq = [
   {
-    question: 'Se eu esquecer algo no carro, como faz?',
-    answer: 'Você deve entrar em contato através do botão "Ajuda" disponível na viagem em que esqueceu o item, está disponível na aba "Corridas" do seu aplicativo.',
+    question: 'Como checo meus ganhos?',
+    answer: "Dentro do aplicativo, você consegue verificar facilmente os seus ganhos. Toque no menu no lado superior esquerdo e depois no ganho, dia e semanal. \n\nNessa seção você terá algumas visualizações: \n\nEstatísticas, ganho de viagens, transações do motorista, ganho diário, transações do motorista e pagamento.",
   },
   {
-    question: 'Se eu esquecer algo no carro, como faz?',
-    answer: 'Você deve entrar em contato através do botão "Ajuda" disponível na viagem em que esqueceu o item, está disponível na aba "Corridas" do seu aplicativo.',
+    question: 'O ano do meu carro é aceito?',
+    answer: 'Serão aprovados todos os modelos de veículos com até 8 anos de fabricação a contar do mês de emissão do CRLV (2014). \nCaso continue com dúvida entre em contato no suporte pelo aplicativo.',
   },
   {
-    question: 'Se eu esquecer algo no carro, como faz?',
-    answer: 'Você deve entrar em contato através do botão "Ajuda" disponível na viagem em que esqueceu o item, está disponível na aba "Corridas" do seu aplicativo.',
+    question: 'Como altero meu e-mail?',
+    answer: 'A alteração do e-mail pode ser feita imediatamente pelo aplicativo. \nSe você ainda tem dúvida sobre essa questão, acesse suporte.',
   },
   {
-    question: 'Se eu esquecer algo no carro, como faz?',
-    answer: 'Você deve entrar em contato através do botão "Ajuda" disponível na viagem em que esqueceu o item, está disponível na aba "Corridas" do seu aplicativo.',
+    question: 'Como altero o telefone?',
+    answer: 'A alteração de telefone pode ser feita imediatamente pelo aplicativo. \n\nAcesse o menu no canto superior esquerdo da tela inicial do seu aplicativo para acessar o seu perfil. Toque em perfil Atualize informações. Pronto, seu telefone já será atualizado no aplicativo! \n\nMas lembre-se que, após a alteração, você será desconectado de seu perfil e será necessário realizar o novo login no App. \n\nSe você ainda tem dúvida sobre essa questão, entre em contato com o suporte.',
   },
   {
-    question: 'Se eu esquecer algo no carro, como faz?',
-    answer: 'Você deve entrar em contato através do botão "Ajuda" disponível na viagem em que esqueceu o item, está disponível na aba "Corridas" do seu aplicativo.',
+    question: 'Como altero minha senha do aplicativo?',
+    answer: 'É muito simples, é só seguir os passos abaixo: \n\nAcesse o ícone no canto superior esquerdo da tela inicial do seu aplicativo; \nToque em menu; \nSelecione perfil; \nClique em Alterar senha. \n\nPronto, sua senha já será atualizada no aplicativo e você terá que se logar novamente! Se você ainda tem dúvida sobre essa questão, entre em contato com o suporte.',
   }
 
 ]
